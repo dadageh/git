f0659cfa8261afd4fb87707843bf89adc472bca7
ngwanyu <ngwanyushey093@gmail.com>
2023-10-06 15:18:40 +0200

    managers_list created

---
+++ managers_list.sh
@@ -0,0 +1,7 @@
+managers list
+love
+delia
+delphine
+lisa
+class8
+dada
--- mangers_list.sh
+++ mangers_list.sh
@@ -1,6 +1,8 @@
-managers list
-love
-delia
-delphine
-lisa
-class8
+1  managers list
+     2  love
+     3  delia
+     4  delphine
+     5  lisa
+     6  class8
+     7  dada
+
